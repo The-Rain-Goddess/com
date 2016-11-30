1d1529ea9b717c15dd50360b44bab5efe643cd4f
Ryan May <xtheraingoddessx@gmail.com>
2016-11-30 13:10:43 -0800

    Added new class Explorer to classwork, cleaned up Car in classwork

---
+++ com/rain/classwork/Explorer.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.rain.classwork;
+
+import java.util.Scanner;
+
+/**
+ * @author Ryan May
+ *
+ */
+public class Explorer {
+
+	/**
+	 * 
+	 */
+	public Explorer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This was just a test all along...");
+		Scanner kb = new Scanner (System.in);
+		kb.nextLine();
+		kb.close();
+		return;
+	}
+
+}
