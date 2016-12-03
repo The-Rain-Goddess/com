f77c5d4d4fbaa13a7ea70e51261dfbe2e0bcbec7
Ryan May <xtheraingoddessx@gmail.com>
2016-12-03 12:47:34 -0800

    added more get paramaters to take(Egg) in dragcave.WebListener

--- com/rain/web/dragcave/WebListener.java
+++ com/rain/web/dragcave/WebListener.java
@@ -121,7 +121,7 @@ public class WebListener implements Runnable {
 	@SuppressWarnings("unused")
 	public void take(Egg e){
 		if(e.getType().equals("Gold") || e.getType().equals("Ice") || e.getType().equals("Magma") || e.getType().equals("Thunder") || e.getType().equals("Silver")
-				|| e.getType().contains("Dino")){
+				|| e.getType().contains("Dino") || e.getType().equals("Blusang Lindwurm")){
 			String url = "http://dragcave.net/get/" + e.getId().trim();
 			try {
 				Document d = Jsoup.connect(url).get();
