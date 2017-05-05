3793f0c4c5a94572753a07eff31786e1477f2917
The-Rain-Goddess <xtheraingoddessx@gmail.com>
2017-05-04 22:29:57 -0700

    minor text fixes

--- com/rain/app/service/riot/api/endpoints/league/methods/GetLeagueBySummonerId.java
+++ com/rain/app/service/riot/api/endpoints/league/methods/GetLeagueBySummonerId.java
@@ -1,22 +1,21 @@
 package com.rain.app.service.riot.api.endpoints.league.methods;
 
 import java.util.List;
-import java.util.Map;
 
 import com.google.gson.reflect.TypeToken;
 import com.rain.app.service.riot.api.ApiConfig;
 import com.rain.app.service.riot.api.endpoints.league.LeagueApiMethod;
-import com.rain.app.service.riot.api.endpoints.league.dto.League;
-import com.rain.app.service.riot.constant.Region;
+import com.rain.app.service.riot.api.endpoints.league.dto.LeagueList;
+import com.rain.app.service.riot.constant.Platform;
 
 public class GetLeagueBySummonerId extends LeagueApiMethod {
 
-	public GetLeagueBySummonerId(ApiConfig config, Region region, String summonerIds) {
+	public GetLeagueBySummonerId(ApiConfig config, Platform platform, long summonerId) {
 		super(config);
-		setRegion(region);
-		setReturnType(new TypeToken<Map<String, List<League>>>() {
+		setPlatform(platform);
+		setReturnType(new TypeToken<List<LeagueList>>() {
 		}.getType());
-		setUrlBase(region.getEndpoint() + "/v2.5/league/by-summoner/" + summonerIds);
+		setUrlBase(platform.getHost() + "/lol/league/v3/leagues/by-summoner/" + summonerId);
 		addApiKeyParameter();
 	}
 }
