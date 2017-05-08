f0bf0e6b7dda39e0784f3f552eaa2ae77735ed14
Ryan May <xtheraingoddessx@gmail.com>
2017-05-07 18:39:44 -0700

    added mapOfListsToString method for ServerUtility

--- com/rain/app/server/redux/ServerUtilities.java
+++ com/rain/app/server/redux/ServerUtilities.java
@@ -17,7 +17,7 @@ public class ServerUtilities {
 	public static String mapToString(Map<?, ?> map){
 		String re = "";
 		for(Map.Entry<?, ?> entry : map.entrySet()){
-			re = re + entry.toString();
+			re = re + entry.toString() + "\n";
 		} return re;
 	}
 	
@@ -27,5 +27,14 @@ public class ServerUtilities {
 			list.add(entry.getKey().toString() + ":" + entry.getValue().toString());
 		} return list;
 	}
-
+	
+	public static String mapOfListsToString(Map<Integer, List<AggregatedChampionData>> rankedChampionDataMap){
+		String r = "";
+		for(Entry<Integer, List<AggregatedChampionData>> entry : rankedChampionDataMap.entrySet()){
+			r = r + entry.getKey() + ":\n";
+			for(Object s : entry.getValue()){
+				r = r + s.toString() + "\n";
+			}
+		} return r;
+	}
 }
