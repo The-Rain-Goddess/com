d3126ee63bd5c973692b2046661ba8aae2e1beee
Ryan May <xtheraingoddessx@gmail.com>
2017-05-20 14:18:19 -0700

    homework for class

---
+++ com/rain/classwork/MidTerm2.java
@@ -0,0 +1,17 @@
+package com.rain.classwork;
+
+public class MidTerm2 {
+
+	public static void main(String[] args){
+		String s1 = "Kristine";
+		s1 += " Thomas";
+		String s2 = s1;
+		if (s1.equals(s2)) {
+		    System.out.println("same");
+		} 
+		if (s1 == s2) {
+		    System.out.println("equal");
+		}
+	}
+
+}
